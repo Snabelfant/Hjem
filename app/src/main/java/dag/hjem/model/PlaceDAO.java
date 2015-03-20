f36dcb47aa672ef9208d930cb85d43146a076c10
Krokofant <dmvv@start.no>
2015-03-20 19:48:57 +0100

    zzz

--- app/src/main/java/dag/hjem/model/PlaceDAO.java
+++
@@ -1,7 +0,0 @@
-package dag.hjem.model;
-
-/**
- * Created by Dag on 12.03.2015.
- */
-public class PlaceDAO {
-}
--- app/src/main/java/dag/hjem/model/travelproposal/PlaceDAOImpl.java
+++
@@ -1,7 +0,0 @@
-package dag.hjem.model.travelproposal;
-
-/**
- * Created by Dag on 12.03.2015.
- */
-public class PlaceDAOImpl {
-}
